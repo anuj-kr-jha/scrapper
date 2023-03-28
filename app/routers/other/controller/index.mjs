348a270db646cffebc7bf7773e1bd2839a41ea82
Anuj Kumar Jha <anuj.kr.jha@protonmail.com>
2023-03-29 00:57:49 +0530

    added new api for ig myfxbook and dailyfx

--- app/routers/other/controller/index.mjs
+++ app/routers/other/controller/index.mjs
@@ -31,7 +31,21 @@ export const c = {
     }
   },
   //
-  recent: async (req, res, next) => {
+  recent_old: async (req, res, next) => {
+    try {
+      const result = calculate().map((item) => {
+        const { long, short, ...rest } = item;
+        return rest;
+      });
+      req.data = { err: false, _data: { FX: result }, info: '' };
+    } catch (err) {
+      req.data = { err: true, _data: null, info: err.message };
+      db.get('ERROR').splice(9, 1, { message: err.message, createdAt: new Date().toISOString(), trace: err.stack }).write();
+    } finally {
+      return next();
+    }
+  },
+  calculated: async (req, res, next) => {
     try {
       const result = calculate();
       req.data = { err: false, _data: { FX: result }, info: '' };
@@ -42,13 +56,52 @@ export const c = {
       return next();
     }
   },
-  recent_old: async (req, res, next) => {
+  ig: async (req, res, next) => {
     try {
-      const result = calculate().map((item) => {
-        const { long, short, ...rest } = item;
-        return rest;
-      });
-      req.data = { err: false, _data: { FX: result }, info: '' };
+      const IG = db.get('RAW_IG').map((x) => ({
+        currency: x.currency,
+        percent: x.percent,
+        longShort: x.longShort,
+        status: x.status,
+        createdAt: x.createdAt,
+      }));
+      req.data = { err: false, _data: { IG }, info: '' };
+    } catch (err) {
+      req.data = { err: true, _data: null, info: err.message };
+      db.get('ERROR').splice(9, 1, { message: err.message, createdAt: new Date().toISOString(), trace: err.stack }).write();
+    } finally {
+      return next();
+    }
+  },
+  myfxbook: async (req, res, next) => {
+    try {
+      const MYFXBOOK = db.get('RAW_MYFXBOOK').map((x) => ({
+        currency: x.currency,
+        shortPercent: x.shortPercent,
+        status: x.status,
+        createdAt: x.createdAt,
+      }));
+      req.data = { err: false, _data: { MYFXBOOK }, info: '' };
+    } catch (err) {
+      req.data = { err: true, _data: null, info: err.message };
+      db.get('ERROR').splice(9, 1, { message: err.message, createdAt: new Date().toISOString(), trace: err.stack }).write();
+    } finally {
+      return next();
+    }
+  },
+  dailyfx: async (req, res, next) => {
+    try {
+      const DAILYFX = db.get('RAW_DAILYFX').map((x) => ({
+        currency: x.currency,
+        trading_bias: x.trading_bias,
+        net_long_percent: x.net_long_percent,
+        net_short_percent: x.net_short_percent,
+        change_in_longs: x.change_in_longs,
+        change_in_shorts: x.change_in_shorts,
+        change_in_oi: x.change_in_oi,
+        createdAt: x.createdAt,
+      }));
+      req.data = { err: false, _data: { DAILYFX }, info: '' };
     } catch (err) {
       req.data = { err: true, _data: null, info: err.message };
       db.get('ERROR').splice(9, 1, { message: err.message, createdAt: new Date().toISOString(), trace: err.stack }).write();
--- app/routers/other/index.mjs
+++ app/routers/other/index.mjs
@@ -6,7 +6,10 @@ const router = Router();
 
 //base: other
 router.get('/ping', c.ping, c.send);
-router.get('/recent', c.recent, c.json);
 router.get('/recent_old', c.recent_old, c.json);
+router.get('/recent/calculated', c.calculated, c.json);
+router.get('/recent/ig', c.ig, c.json);
+router.get('/recent/myfxbook', c.myfxbook, c.json);
+router.get('/recent/dailyfx', c.dailyfx, c.json);
 
 export { router as otherRoute };
